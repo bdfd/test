cf875207f816e8676e190ffffc092b3fe26473a3
Kelvin Chen <bdfd2005@gmail.com>
2020-02-25 15:27:49 -0500

    1

--- 爱前端node.js实战开发/routes/index.js
+++ 爱前端node.js实战开发/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/student/:xuehao', function(req, res) {
+  if (/^[\d]{6}$/.test(req.params.xuehao)) {
+    res.send('学生频道，学号' + req.params.xuehao);
+  } else {
+    res.send('学号不正确!');
+  }
+});
+
+// router.get('/student/:xuehao'),
+//   function(req, res) {
+//     if (/^[\d]{6}$/.test(req.params.xuehao)) {
+//       res.send('学生频道，学号' + req.params.xuehao);
+//     } else {
+//       res.send('学号不正确!');
+//     }
+//   };
+
 module.exports = router;
