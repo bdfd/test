b0ad53cd955c4fe52893d0f053562bba7c9299d9
Kelvin Chen <bdfd2005@gmail.com>
2020-03-06 20:17:02 -0500

    update 03-06-2020

---
+++ Javascript Functional Programming Test/public/javascripts/tutorial/03.js
@@ -0,0 +1,14 @@
+var Linkedlist = function() {
+  //链表头
+  var head = null; //Object null
+  //链表长度
+  var length = 0;
+
+  var Node = function(element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  //链表尾添加元素
+  this.append = function(element) {};
+};
