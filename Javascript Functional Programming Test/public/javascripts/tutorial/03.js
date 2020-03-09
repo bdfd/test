a7e3ee4fb8080f97bf16f2df6c4dd17b14d2489b
Kelvin Chen <bdfd2005@gmail.com>
2020-03-09 13:21:33 -0400

    udpate on 03-09-2020

--- Javascript Functional Programming Test/public/javascripts/tutorial/03.js
+++ Javascript Functional Programming Test/public/javascripts/tutorial/03.js
@@ -1,4 +1,4 @@
-var Linkedlist = function() {
+var LinkedList = function() {
   //链表头
   var head = null; //Object null
   //链表长度
@@ -10,5 +10,27 @@ var Linkedlist = function() {
   };
 
   //链表尾添加元素
-  this.append = function(element) {};
+  this.append = function(element) {
+    var node = new Node(element);
+    // node = {
+    // element : element
+    // next : null
+    if (head == null) {
+      head = node;
+    } else {
+      var current = head;
+      while (current.next) {
+        current = current.next;
+      }
+      //当while循环执行完之后, current已经是链表最后一项了
+      current.next = node;
+    }
+    length++;
+  };
+  //链表某一个位置添加元素
+
+  //显示链表头元素
+  this.getHead = function() {
+    return head;
+  };
 };
