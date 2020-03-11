1e548c8fd9f7ee9a9f96a228d32c4412c77343eb
Kelvin Chen <bdfd2005@gmail.com>
2020-03-11 13:56:25 -0400

    1

--- Javascript Functional Programming Test/routes/ES6.js
+++ Javascript Functional Programming Test/routes/ES6.js
@@ -8,11 +8,45 @@ router.get('/index', function(req, res, next) {
   res.render('functions/es6/index');
 });
 
-//router address localhost:2001/func_fundamental/01
+//router address localhost:2001/func_es6/01
 //descriptions: Javascript Lecture 1
 //comments:
-router.get('/index', function(req, res, next) {
-  res.render('functions/es6/index');
+router.get('/01', function(req, res, next) {
+  res.render('functions/es6/es6_01');
 });
 
+//router address localhost:2001/func_es6/02
+//descriptions: Javascript Lecture 2
+//comments:
+router.get('/02', function(req, res, next) {
+  res.render('functions/es6/es6_02');
+});
+
+//router address localhost:2001/func_es6/03
+//descriptions: Javascript Lecture 3
+//comments:
+router.get('/03', function(req, res, next) {
+  res.render('functions/es6/es6_03');
+});
+
+//router address localhost:2001/func_es6/04
+//descriptions: Javascript Lecture 4
+//comments:
+router.get('/04', function(req, res, next) {
+  res.render('functions/es6/es6_04');
+});
+
+//router address localhost:2001/func_es6/05
+//descriptions: Javascript Lecture 5
+//comments:
+router.get('/05', function(req, res, next) {
+  res.render('functions/es6/es6_05');
+});
+
+//router address localhost:2001/func_es6/06
+//descriptions: Javascript Lecture 6
+//comments:
+router.get('/06', function(req, res, next) {
+  res.render('functions/es6/es6_06');
+});
 module.exports = router;
